0134fa144224e8c284cd447ad01f085c7db2992e
ppqqmmcc737084-ui <ppqqmmcc737084@gmail.com>
2026-04-06 22:48:58 +0000

    إصلاح namespace لمكتبة البلوتوث بلغة Kotlin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,24 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+// ✅ التعديل السحري بلغة Kotlin لحل مشكلة namespace لمكتبة البلوتوث
+subprojects {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            val androidExt = extensions.findByName("android")
+            if (androidExt != null) {
+                try {
+                    val getNamespace = androidExt.javaClass.getMethod("getNamespace")
+                    val namespace = getNamespace.invoke(androidExt)
+                    if (namespace == null) {
+                        val setNamespace = androidExt.javaClass.getMethod("setNamespace", String::class.java)
+                        setNamespace.invoke(androidExt, project.group.toString())
+                    }
+                } catch (e: Exception) {
+                    // تجاهل
+                }
+            }
+        }
+    }
+}
